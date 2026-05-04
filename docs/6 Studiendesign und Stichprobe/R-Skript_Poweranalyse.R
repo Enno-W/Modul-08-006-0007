774a103415eb91300504d0f22540fa032b645f9d
Enno-W <ennomail95@yahoo.de>
2026-05-04 17:36:11 +0800

    R Skript für LV6

---
+++ docs/6 Studiendesign und Stichprobe/R-Skript_Poweranalyse.R
@@ -0,0 +1,22 @@
+# Power-Analyse mit R
+
+# Installation 
+install.packages("pwr")
+#Laden des Pakets
+library(pwr)
+
+#t- Test
+##Wie viele Studienteilnehmer brauchen wir für einen mittleren Effekt (d = 0.5) im t-Test?
+pwr.t.test(d = 0.5, sig.level = 0.05, power = 0.8, 
+           type = "two.sample", alternative = "two.sided")
+
+##Korrelation zwischen Selbstwirksamkeit und PA (r = 0.3)
+pwr.r.test(r = 0.3, sig.level = 0.05, power = 0.8)
+
+# Anova gemäß @Cohen1988
+#Vergleich von 4 Interventionsgruppen je 20 Personen, Effektstärke f=.28
+pwr.anova.test(f=0.28,k=4,n=20,sig.level=0.05)
+
+# Chi-Square-Test gemäß @Cohen1988
+##Test auf Unabhängigkeit von kategorialen Variablen (z.B. Geschlecht vs. Sportstatus).
+pwr.chisq.test(w=0.346,df=(2-1)*(3-1),N=140,sig.level=0.01)
